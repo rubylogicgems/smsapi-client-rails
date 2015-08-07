a92acef3c6c56620cf0e1fd0cdc356fe942212d4
Marek <marekmachula82@gmail.com>
2015-08-07 15:57:34 +0200

    Smsapi config

---
+++ lib/generators/smsapi/smsapi_config_generator.rb
@@ -0,0 +1,19 @@
+# This generator drops the smsapi_config.rb and smsapi_config.yml file
+# into the appropriate places in a rails app to configure and initialize the
+# api credentials.
+module SMSapi
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      desc <<DESC
+Description:
+    Copies Smsapi configuration files to your application's config directory.
+DESC
+      def copy_config_file
+        template 'smsapi.rb', 'config/initializers/smsapi.rb'
+        template 'smsapi.rb', 'config/smsapi.yml'
+      end
+    end
+  end
+end
--- smsapi_client_rails.gemspec
+++ smsapi_client_rails.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |s|
   s.executables   = s.files.grep(%r{^exe/}) { |f| File.basename(f) }
   s.require_paths = ["lib"]
 
+  s.add_dependency 'activesupport', ['>= 3.0.0']
   s.add_development_dependency "bundler", "~> 1.8"
   s.add_development_dependency "rake", "~> 10.0"
 end
