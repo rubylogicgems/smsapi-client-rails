fd160274386bac46a942d8ea0e8a6a5df2025982
Marek <marekmachula82@gmail.com>
2015-08-21 11:18:42 +0200

    QF

--- lib/generators/smsapi/templates/smsapi.rb
+++ lib/generators/smsapi/templates/smsapi.rb
@@ -3,4 +3,6 @@ api_key = YAML.load_file(Rails.root.join('config', 'smsapi.yml'))
 SMSapi.configure do |config|
   config.username = api_key['username']
   config.password = api_key['password']
+  config.test = api_key['test']
+  config.flash = api_key['flash']
 end
