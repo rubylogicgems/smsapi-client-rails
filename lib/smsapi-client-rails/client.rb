5df3add5a901b70f101911035b907da8115dddca
Marcin Drozd <marcindrozd@me.com>
2015-10-05 17:52:22 +0200

    CR fixes

--- lib/smsapi-client-rails/client.rb
+++ lib/smsapi-client-rails/client.rb
@@ -1,33 +1,30 @@
 module SmsapiRails
 
   class Client
+    attr_reader :client
+
     def initialize(username = SMS_API['username'], password = SMS_API['password'])
       @client = Smsapi::Client.new(username, password)
     end
 
     def send_sms(to, message, options = {})
-      result = @client.send_single(to, message, options)
-      result
+      client.send_single(to, message, options)
     end
 
-    def schedule_single(to, message, date, options = {})
-      result = @client.schedule_single(to, message, date, options)
-      result
+    def schedule_sms(to, message, date, options = {})
+      client.schedule_single(to, message, date, options)
     end
 
     def send_bulk(to, message, options = {})
-      result = @client.send_bulk(to, message, options)
-      result
+      client.send_bulk(to, message, options)
     end
   end
 
   def self.send_sms(to, message, options = {})
-    result = SmsapiRails::Client.new.send_sms(to, message, options)
-    result
+    SmsapiRails.client.send_single(to, message, options)
   end
 
   def self.send_bulk(to, message, options = {})
-    result = SmsapiRails::Client.new.send_bulk(to, message, options)
-    result
+    SmsapiRails.client.send_bulk(to, message, options)
   end
 end
